2fccf1e25e96c8b9e078ed211e03a9e82f53feed
cominetor <emanuele.cominato@gmail.com>
2025-05-01 20:51:16 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-
-const CACHE='bcn-full-v1';
-const CORE=['.','index.html','script.js','places.json','itinerary.json','manifest.json'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
